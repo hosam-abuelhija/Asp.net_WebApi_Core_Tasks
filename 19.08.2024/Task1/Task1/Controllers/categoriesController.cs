52aca4d7ecb7cb12c3c11e86c1c768f16b6a681a
hosam-abuelhija <hosam.adnan123@gmail.com>
2024-08-19 17:13:30 +0300

    task1

---
+++ 19.08.2024/Task1/Task1/Controllers/categoriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class categoriesController : ControllerBase
+    {
+        private MyDbContext _dbContext;
+        public categoriesController(MyDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        [HttpGet]
+        public IActionResult Categories()
+        {
+            var data = _dbContext.Categories.ToList();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Categories(int id)
+        {
+            var data = _dbContext.Categories.Find(id);
+            return Ok(data);
+        }
+
+
+
+
+    }
+}
---
+++ 19.08.2024/Task1/Task1/Controllers/productsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productsController : ControllerBase
+    {
+        private MyDbContext _dbContext;
+        public productsController(MyDbContext db)
+        {
+            _dbContext = db;
+        }
+
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            var data = _dbContext.Products.ToList();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Products(int id)
+        {
+            var data = _dbContext.Products.Find(id);
+            return Ok(data);
+        }
+    }
+}
